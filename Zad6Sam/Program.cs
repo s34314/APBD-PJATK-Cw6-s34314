c990bf0d84441af82aff0dc2e0ea05438f7ffe19
s34314 <s34314@pjwstk.edu.pl>
2026-04-26 23:55:12 +0200

    Wprowadzenie Main i appsettings.json

--- Zad6Sam/Program.cs
+++ Zad6Sam/Program.cs
@@ -1,32 +1,17 @@
-namespace Zad6Sam;
+using ClinicApi.Services;
 
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<AppointmentService>();
 
-        builder.Services.AddControllers();
-        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-        builder.Services.AddOpenApi();
+var app = builder.Build();
 
-        var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
 
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.MapOpenApi();
-        }
+app.MapControllers();
 
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-
-        app.MapControllers();
-
-        app.Run();
-    }
-}
+app.Run();
